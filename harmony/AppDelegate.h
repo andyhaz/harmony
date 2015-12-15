2c538edba0dd42fa38c238b4f7d3d2187b42269e
andy Hazlett <andyhaz1970@gmail.com>
2015-12-15 14:44:45 -0800

    Initial Commit

---
+++ harmony/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  harmony
+//
+//  Created by andrew hazlett on 12/15/15.
+//  Copyright © 2015 andrew hazlett. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ harmony/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  harmony
+//
+//  Created by andrew hazlett on 12/15/15.
+//  Copyright © 2015 andrew hazlett. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
