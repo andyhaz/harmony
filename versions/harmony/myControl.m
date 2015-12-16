646a8621e5c38a89cc681f8da7fc1fa1064ccf74
andy Hazlett <andyhaz1970@gmail.com>
2015-12-15 17:06:59 -0800

    add icons

---
+++ versions/harmony/myControl.m
@@ -0,0 +1,17 @@
+//
+//  myControl.m
+//  harmony
+//
+//  Created by andrew hazlett on 12/15/15.
+//  Copyright © 2015 andrew hazlett. All rights reserved.
+//
+
+#import "myControl.h"
+
+@implementation myControl {
+    IBOutlet myView *myCustomViewObjOutlet;
+    IBOutlet NSTextField *myTextFieldOutlet;
+}
+
+
+@end
---
+++ versions/harmony/myView.m
@@ -0,0 +1,19 @@
+//
+//  myView.m
+//  harmony
+//
+//  Created by andrew hazlett on 12/15/15.
+//  Copyright © 2015 andrew hazlett. All rights reserved.
+//
+
+#import "myView.h"
+
+@implementation myView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
