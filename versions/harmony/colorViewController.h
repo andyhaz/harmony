fa78a6a3f0691fd79404671e0d6b0df75bbb643d
andy Hazlett <andyhaz1970@gmail.com>
2016-02-17 23:47:04 -0800

    add full color window support

--- versions/harmony/colorViewController.h
+++ versions/harmony/colorViewController.h
@@ -9,11 +9,7 @@
 #import <Cocoa/Cocoa.h>
 
 @interface colorViewController : NSWindowController{
-    
-    IBOutlet NSTextField *myFillFieldOutlet;
-    IBOutlet NSTextField *myOutlineOutlet;
-    IBOutlet NSTextField *myBackgoundOutlet;
-    IBOutlet NSTextField *myTextOutlet;
+
 }
 
 @property (nonatomic,retain) NSMutableArray *myColorData;
@@ -23,6 +19,22 @@
 @property (weak) IBOutlet NSTextField *outlineInfoFieldOutlet;
 @property (weak) IBOutlet NSTextField *bgInfoFieldOutlet;
 @property (weak) IBOutlet NSTextField *textInfoFieldOutlint;
+
+@property (weak) IBOutlet NSTextField *RedBGInfoField;
+@property (weak) IBOutlet NSTextField *BlueBGInfoField;
+@property (weak) IBOutlet NSTextField *GreenBGInfoField;
+
+@property (weak) IBOutlet NSTextField *RedFillInfoField;
+@property (weak) IBOutlet NSTextField *BlueFillInfoField;
+@property (weak) IBOutlet NSTextField *GreenFillInfoField;
+
+@property (weak) IBOutlet NSTextField *RedOutlineInfoField;
+@property (weak) IBOutlet NSTextField *BlueOutlineInfoField;
+@property (weak) IBOutlet NSTextField *GreenOutlineInfoField;
+
+@property (weak) IBOutlet NSTextField *RedTextInfoField;
+@property (weak) IBOutlet NSTextField *BlueTextInfoField;
+@property (weak) IBOutlet NSTextField *GreenTextInfoField;
 
 -(void)showData;
 
--- versions/harmony/colorViewController.m
+++ versions/harmony/colorViewController.m
@@ -16,7 +16,7 @@
 
 @synthesize myColorData,myColorHex;
 @synthesize fillInfoField,outlineInfoFieldOutlet,bgInfoFieldOutlet,textInfoFieldOutlint;
-
+@synthesize RedBGInfoField,BlueBGInfoField,GreenBGInfoField,RedFillInfoField,BlueFillInfoField,GreenFillInfoField,RedOutlineInfoField,BlueOutlineInfoField,GreenOutlineInfoField,RedTextInfoField,BlueTextInfoField,GreenTextInfoField;
 - (void)windowDidLoad {
     [super windowDidLoad];
 // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
@@ -26,12 +26,35 @@
 
 -(void)showData {
  //   NSLog(@"show data");
-
     [bgInfoFieldOutlet setStringValue:myColorHex[2]];
     [fillInfoField setStringValue:myColorHex[0]];
     [outlineInfoFieldOutlet setStringValue:myColorHex[1]];
     [textInfoFieldOutlint setStringValue:myColorHex[3]];
     //NSLog(@"%@ - %@",myColorData,myColorHex);
+    
+    [RedBGInfoField setStringValue:[self convertColor:myColorData[8]]];
+    [BlueBGInfoField setStringValue:[self convertColor:myColorData[9]]];
+    [GreenBGInfoField setStringValue:[self convertColor:myColorData[10]]];
+    
+    [RedFillInfoField setStringValue:[self convertColor:myColorData[0]]];
+    [BlueFillInfoField setStringValue:[self convertColor:myColorData[1]]];
+    [GreenFillInfoField setStringValue:[self convertColor:myColorData[2]]];
+    
+    [RedOutlineInfoField setStringValue:[self convertColor:myColorData[4]]];
+    [BlueOutlineInfoField setStringValue:[self convertColor:myColorData[5]]];
+    [GreenOutlineInfoField setStringValue:[self convertColor:myColorData[6]]];
+    
+    [RedTextInfoField setStringValue:[self convertColor:myColorData[12]]];
+    [BlueTextInfoField setStringValue:[self convertColor:myColorData[13]]];
+    [GreenTextInfoField setStringValue:[self convertColor:myColorData[14]]];
+
+}
+
+-(NSString*)convertColor :(NSString*)strColor{
+    float colorNum = [strColor floatValue];
+    float newColor = colorNum*255;
+    
+    return [NSString stringWithFormat:@"%f",newColor];
 }
 
 @end
